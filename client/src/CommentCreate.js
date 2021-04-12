d0e8c8337ec3d640cf64c1cda1375c038810e9d0
Semal10 <semal.sherathia@gmail.com>
2021-04-12 20:38:52 +0530

    Initial Commit

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,28 @@
+import React , {useState} from 'react';
+import axios from 'axios';
+
+const CommentCreate = ({postId}) => {
+    const [content , setContent] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await axios.post(`http://localhost:4001/posts/${postId}/comments` , {
+            content
+        });
+        setContent('');
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>New Comment</label>
+                    <input value={content} onChange={e => setContent(e.target.value)} type="text" className="form-control"/>
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default CommentCreate;
